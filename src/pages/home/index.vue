418fa00234afa6ee0e2c44f6565e3a4773a8a2bf
ysy <2398581361@qq.com>
2024-09-25 00:50:25 +0800

    fix:/home(首页去除搜索框)

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="viewcontent">
     <div v-show="usetsto.active === 'index'" class="index-page">
-      <!-- <Index /> -->
-      <CoachHome />
+      <Index />
+      <!-- <CoachHome /> -->
     </div>
     <div v-show="usetsto.active === 'action'" class="action-page">
       <Action />
@@ -11,81 +11,56 @@
       <Mine />
     </div>
   </div>
-  <van-tabbar
-    placeholder
-    style="margin-bottom: 15px; margin-top: 15px; padding-top: 15px"
-    @change="onChange"
-    v-model="usetsto.getcurrentactive"
-  >
-    <van-tabbar-item
-      :name="tabbardata[0].name"
-      style="margin-bottom: 15px; height: 100%"
-      v-if="usetsto.active == tabbardata[0].name"
-    >
+  <van-tabbar placeholder style="margin-bottom: 15px; margin-top: 15px; padding-top: 15px" @change="onChange"
+    v-model="usetsto.getcurrentactive">
+    <van-tabbar-item :name="tabbardata[0].name" style="margin-bottom: 15px; height: 100%"
+      v-if="usetsto.active == tabbardata[0].name">
       <span :style="{ color: tabbardata[0].textactivecolor }">{{
-        tabbardata[0].title
-      }}</span>
+      tabbardata[0].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[0].activeicon" />
       </template>
     </van-tabbar-item>
-    <van-tabbar-item
-      style="margin-bottom: 15px; height: 100%"
-      :name="tabbardata[0].name"
-      v-else
-    >
+    <van-tabbar-item style="margin-bottom: 15px; height: 100%" :name="tabbardata[0].name" v-else>
       <span :style="{ color: tabbardata[0].textcolor }">{{
-        tabbardata[0].title
-      }}</span>
+      tabbardata[0].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[0].inactiveicon" />
       </template>
     </van-tabbar-item>
-    <van-tabbar-item
-      style="margin-bottom: 15px; height: 100%"
-      :name="tabbardata[1].name"
-      v-if="usetsto.active == tabbardata[1].name"
-    >
+    <van-tabbar-item style="margin-bottom: 15px; height: 100%" :name="tabbardata[1].name"
+      v-if="usetsto.active == tabbardata[1].name">
       <span :style="{ color: tabbardata[1].textactivecolor }">{{
-        tabbardata[1].title
-      }}</span>
+      tabbardata[1].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[1].activeicon" />
       </template>
     </van-tabbar-item>
-    <van-tabbar-item
-      style="margin-bottom: 15px; height: 100%"
-      :name="tabbardata[1].name"
-      v-else
-    >
+    <van-tabbar-item style="margin-bottom: 15px; height: 100%" :name="tabbardata[1].name" v-else>
       <span :style="{ color: tabbardata[1].textcolor }">{{
-        tabbardata[1].title
-      }}</span>
+      tabbardata[1].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[1].inactiveicon" />
       </template>
     </van-tabbar-item>
 
-    <van-tabbar-item
-      style="margin-bottom: 15px; height: 100%"
-      :name="tabbardata[2].name"
-      v-if="usetsto.active == tabbardata[2].name"
-    >
+    <van-tabbar-item style="margin-bottom: 15px; height: 100%" :name="tabbardata[2].name"
+      v-if="usetsto.active == tabbardata[2].name">
       <span :style="{ color: tabbardata[2].textactivecolor }">{{
-        tabbardata[2].title
-      }}</span>
+      tabbardata[2].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[2].activeicon" />
       </template>
     </van-tabbar-item>
-    <van-tabbar-item
-      style="margin-bottom: 15px; height: 100%"
-      :name="tabbardata[2].name"
-      v-else
-    >
+    <van-tabbar-item style="margin-bottom: 15px; height: 100%" :name="tabbardata[2].name" v-else>
       <span :style="{ color: tabbardata[2].textcolor }">{{
-        tabbardata[2].title
-      }}</span>
+      tabbardata[2].title
+    }}</span>
       <template #icon>
         <img class="w-7 h-7" :src="tabbardata[2].inactiveicon" />
       </template>
