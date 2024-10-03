ed30b43ba5aeb3034f6622444cd850556aca09c5
ysy <2398581361@qq.com>
2024-10-03 08:22:27 +0800

    fix:修复mine页面条件渲染

--- src/components/mine/index.vue
+++ src/components/mine/index.vue
@@ -4,146 +4,84 @@
       <img class="w-screen z-0 absolute top-0" src="../../static/wave-bg.png" />
 
       <div class="user-info z-10 p-5 w-full h-32 flex items-center">
-        <view
-          class="cu-avatar round lg"
-          :style="{ backgroundImage: `url(${userAvatar})` }"
-        >
-        </view>
+        <view class="cu-avatar round lg" :style="{ backgroundImage: `url(${userAvatar})` }"></view>
         <div class="flex flex-wrap z-10">
           <div class="text-xl w-full z-10 text-white pl-4">
-            {{ user.Username }}
-            <text
-              v-if="user.Sex === 1"
-              style="font-size: 25px; color: #a54aff"
-              class="cuIcon-female w-10 h-10 text-2xl text-red margin-right-xs"
-            ></text>
+            {{ user.name }}
+            <text v-if="user.Sex === 1" style="font-size: 25px; color: #a54aff"
+              class="cuIcon-female w-10 h-10 text-2xl text-red margin-right-xs"></text>
 
-            <text
-              style="font-size: 25px; color: #16a9fa"
-              class="cuIcon-male w-10 h-10 text-2xl text-red margin-right-xs"
-            ></text>
+            <text style="font-size: 25px; color: #16a9fa"
+              class="cuIcon-male w-10 h-10 text-2xl text-red margin-right-xs"></text>
           </div>
-          <p class="text-sm z-10 text-white pl-4">20岁</p>
+          <p class="text-sm z-10 text-white pl-4">{{ user.Age }}</p>
         </div>
       </div>
     </div>
 
-    <div
-      style="top: 100px"
-      class="mine-indicators absolute w-full h-40 flex justify-center items-center"
-    >
-      <div
-        style="width: 90%"
-        class="shadow-md rounded-lg px-10 bg-white flex justify-around flex-row py-5"
-      >
-        <div class="mine-indicator bg-white">
-          <div class="indicator-left flex flex-col">
-            <span class="title text-gray-500 tracking-wider mb-3">{{
-              user.RoleName === "student" ? "身高" : "售课次数"
-            }}</span>
-            <span class="num text-2xl text-black font-bold tracking-wider">{{
-              showTab1
-            }}</span>
-          </div>
-
-          <div class="indicator-right"></div>
-        </div>
-        <!-- <div class="mine-indicator bg-white" v-else>
+    <div style="top: 100px" class="mine-indicators absolute w-full h-40 flex justify-center items-center">
+      <div style="width: 90%" class="shadow-md rounded-lg px-10 bg-white flex justify-around flex-row py-5">
+        <div class="mine-indicator bg-white" v-if="user.RoleName === 'student'">
           <div class="indicator-left flex flex-col">
             <span class="title text-gray-500 tracking-wider mb-3">身高</span>
-            <span class="num text-2xl text-black font-bold tracking-wider"
-              >{{ height }}cm</span
-            >
-          </div>
-
-          <div class="indicator-right"></div>
-        </div> -->
-        <div
-          class="mine-indicator bg-white"
-          v-if="(state.RoleName = 'student')"
-        >
-          <div class="indicator-left flex flex-col">
-            <span class="title text-gray-500 tracking-wider mb-3">{{
-              user.RoleName === "student" ? "体重" : "学员数量"
-            }}</span>
-            <span class="num text-2xl text-black font-bold tracking-wider">{{
-              showTab2
-            }}</span>
+            <span class="num text-2xl text-black font-bold tracking-wider">{{ showTab1 }}cm</span>
           </div>
         </div>
         <div class="mine-indicator bg-white" v-else>
           <div class="indicator-left flex flex-col">
+            <span class="title text-gray-500 tracking-wider mb-3">售课次数</span>
+            <span class="num text-2xl text-black font-bold tracking-wider">{{ showTab1 }}</span>
+          </div>
+        </div>
+        <div class="mine-indicator bg-white" v-if="user.RoleName === 'student'">
+          <div class="indicator-left flex flex-col">
             <span class="title text-gray-500 tracking-wider mb-3">体重</span>
-            <span class="num text-2xl text-black font-bold tracking-wider"
-              >{{ weight }}kg</span
-            >
+            <span class="num text-2xl text-black font-bold tracking-wider">{{ showTab2 }}kg</span>
+          </div>
+        </div>
+        <div class="mine-indicator bg-white" v-else>
+          <div class="indicator-left flex flex-col">
+            <span class="title text-gray-500 tracking-wider mb-3">学员数量</span>
+            <span class="num text-2xl text-black font-bold tracking-wider">{{ showTab2 }}</span>
           </div>
         </div>
       </div>
     </div>
 
     <div class="mine-content">
-      <view
-        style="display: flex; justify-content: center"
-        class="flex-wrap cu-list menu text-left"
-      >
-        <view
-          @click="exitinfo"
-          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow"
-        >
+      <view style="display: flex; justify-content: center" class="flex-wrap cu-list menu text-left">
+        <view @click="exitinfo" class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow">
           <view class="content py-3 flex flex-row items-center">
-            <text
-              style="font-size: 25px; color: #ff5e3a"
-              class="cuIcon-circlefill text-grey"
-            ></text>
-
+            <text class="cuIcon-circlefill text-grey"></text>
             <div class="flex flex-col ml-3">
               <text class="text-[#042c5c] text-xl font-bold">设置个人信息</text>
               <text class="text-gray-500">设置头像和昵称</text>
             </div>
           </view>
         </view>
-        <view
-          @click="orderinfo"
-          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow"
-        >
+        <view v-if="user.RoleName === 'student'" @click="orderinfo"
+          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow">
           <view class="content py-3 flex flex-row items-center">
-            <text
-              style="font-size: 25px; color: #ff5e3a"
-              class="cuIcon-circlefill text-grey"
-            ></text>
-
+            <text class="cuIcon-circlefill text-grey"></text>
             <div class="flex flex-col ml-3">
               <text class="text-[#042c5c] text-xl font-bold">我的订单</text>
-              <text class="text-gray-500">查看订单状态 </text>
+              <text class="text-gray-500">查看订单状态</text>
             </div>
           </view>
         </view>
-
-        <view
-          @click="bodyinfo"
-          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow"
-        >
+        <view @click="bodyinfo" class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow">
           <view class="content py-3 flex flex-row items-center">
-            <text
-              style="font-size: 25px; color: #ff5e3a"
-              class="cuIcon-circlefill text-grey"
-            ></text>
+            <text class="cuIcon-circlefill text-grey"></text>
             <div class="flex flex-col ml-3">
               <text class="text-[#042c5c] text-xl font-bold">身体指标</text>
               <text class="text-gray-500">查看我的身体指标</text>
             </div>
           </view>
         </view>
-        <view
-          @click="diyTab"
-          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow"
-        >
+        <view v-if="user.RoleName === 'coach'" @click="diyTab"
+          class="cu-item my-3 w-11/12 shadow-sm p-3 py-5 rounded-xl arrow">
           <view class="content py-3 flex flex-row items-center">
-            <text
-              style="font-size: 25px; color: #ff5e3a"
-              class="cuIcon-circlefill text-grey"
-            ></text>
+            <text class="cuIcon-circlefill text-grey"></text>
             <div class="flex flex-col ml-3">
               <text class="text-[#042c5c] text-xl font-bold">自定义课程</text>
               <text class="text-gray-500">自定义课程内容</text>
@@ -154,37 +92,47 @@
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
+import { computed, onMounted, ref } from 'vue';
 import { getCourseAndStudentCount } from "@/api/coach";
 import { getHWInfo } from "@/api/user";
 import { useAuthStore } from "@/state/modules/auth";
 import { useRouter } from "uni-mini-router";
+import { getUserInfo } from "@/api/user";
 const router = useRouter();
 const authStore = useAuthStore();
 const user = computed(() => authStore.user);
-const userAvatar = ref<string>(
-  "https://ossweb-img.qq.com/images/lol/img/champion/Taric.png"
-);
+const userAvatar = ref<string>("https://ossweb-img.qq.com/images/lol/img/champion/Taric.png");
 const showTab1 = ref<string>("0");
 const showTab2 = ref<string>("0");
+const roleName = ref<string>("student");
 onMounted(() => {
-  console.log("user", user);
-  userAvatar.value = user.value.Avatar;
+  roleName.value = getUserInfo();
+  userAvatar.value = user.Avatar || userAvatar.value;
+  if (roleName.value === "coach") {
 
-  if (user.value.RoleName === "student") {
-    getHWInfo().then((res) => {
-      console.log("-----======", res.data.data);
-      showTab1.value = res.data.data.Height.toString();
-      showTab2.value = res.data.data.Weight.toString();
-    });
-  } else {
     getCourseAndStudentCount().then((res) => {
-      console.log(res);
       showTab1.value = res.data.data.CourseCount.toString();
       showTab2.value = res.data.data.StudentCount.toString();
     });
+
+  } else {
+
+
+    getHWInfo().then((res) => {
+      showTab1.value = res.data.data.Height.toString();
+      showTab2.value = res.data.data.Weight.toString();
+    });
   }
 });
+
+
+const getUserInfo = async () => {
+  const res: any = await getUserInfo()
+  return res.data.data.RoleName ? res.data.data.RoleName : "student";
+};
+
 
 const exitinfo = () => {
   router.push({ name: "info" });
@@ -197,6 +145,7 @@ const orderinfo = () => {
 const bodyinfo = () => {
   router.push({ name: "indicators" });
 };
+
 const diyTab = () => {
   router.push({ name: "diyTable" });
 };
