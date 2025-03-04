a59f9ff2e4dff41df1098e9fb2e26e3b8ea0d7b0
ysy <2398581361@qq.com>
2025-03-05 00:50:45 +0800

    feat: 自定义课程页面样式更改

--- src/subpackages/diyTable/index.vue
+++ src/subpackages/diyTable/index.vue
@@ -1,44 +1,28 @@
 <template>
-  <div class="card" style="background-color: #8080801c">
-    <div
-      class="w-full exhibition-box my-5 relative flex justify-centers rounded-t-[20px] overflow-hidden"
-    >
-      <div class="w-full h-full bg-black absolute z-20 opacity-25"></div>
-      <img
-        class="w-full h-50"
-        src="https://img.js.design/assets/img/668c2ed952a8c9cae69e281b.png#4da10c88bc2ff6efbe0d64ef8f3eee6d"
-      />
+  <div class="w-screen h-screen card-wrap" style="background-image: url('https://zhanjiang-fitness.oss-cn-guangzhou.aliyuncs.com/20250224/1740412443021.png');background-position: center;background-size: cover;">
+    <div class="card" style="background-color: #fff">
       <div
-        class="absolute z-50 text-white bottom-0 w-full p-2 flex-wrap flex justify-center items-center"
-      >
-        <div class="text-left mb-5">
-          “健身不只是身体上的锻炼，它还是心灵和情感的一种训练。”
-        </div>
-        <div class="text-signature mb-2 w-full flex justify-between flex-wrap">
-          <div class="time-text">
-            <span>7月8号 周一 2024</span>
-          </div>
-          <div class="writer-text">
-            <span>亚里士多德</span>
-          </div>
-        </div>
+        class="w-full exhibition-box relative flex justify-centers rounded-t-[20px] overflow-hidden">
+        <img
+          class="w-full h-50"
+          src="https://zhanjiang-fitness.oss-cn-guangzhou.aliyuncs.com/20250305/1741105971033.png" />
       </div>
+      <view class="cu-form-group margin-top" style="width: 100%; background: 0">
+        <view class="label-title">课程数</view>
+        <input placeholder="请输入课程数" name="input" v-model="form.num" />
+      </view>
+      <view class="cu-form-group margin-top" style="width: 100%; background: 0">
+        <view class="label-title">价&nbsp;格</view>
+        <input placeholder="请输入价格" name="input" v-model="form.money" />
+      </view>
+      <button
+        class="cu-btn round large margin-top mb-5"
+        open-type="share"
+        style="background-color:#94d0f2 ;color: #fff;"
+        :disabled="form.money == '' || form.num == ''">
+        点击分享
+      </button>
     </div>
-    <view class="cu-form-group margin-top" style="width: 100%; background: 0">
-      <view class="title">课程数</view>
-      <input placeholder="请输入课程数" name="input" v-model="form.num" />
-    </view>
-    <view class="cu-form-group margin-top" style="width: 100%; background: 0">
-      <view class="title">价&nbsp;格</view>
-      <input placeholder="请输入价格" name="input" v-model="form.money" />
-    </view>
-    <button
-      class="cu-btn bg-blue-200 round large margin-top"
-      open-type="share"
-      :disabled="form.money == '' || form.num == ''"
-    >
-      点击分享
-    </button>
   </div>
 </template>
 <script setup lang="ts">
@@ -66,13 +50,21 @@ const form = reactive<form>({
 });
 </script>
 <style scoped>
+.card-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 .card {
-  margin: 30vw auto;
-  width: 95vw;
-  height: 500px;
+  width: 80vw;
   border-radius: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+}
+.label-title {
+  width: 60px;
 }
 </style>
